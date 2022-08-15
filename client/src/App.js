12e8a812b331a44f76d0cf4ba4967d5b90834af3
d-r-lenin <drlenin2002@gmail.com>
2022-08-15 17:31:52 +0530

    header partial

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import Header from "./components/Header";
+import './css/app.css';
+import Header from "./components/Header.parts/Header";
+import { useMediaQuery } from 'react-responsive';
+import GlobleC from './context';
 
 function App() {
-  return (<>
-    <Header/>
-  </>);
+	const isMobileDevice = useMediaQuery({
+		query: "(max-device-width: 770px)"
+	});;
+
+	return (
+	<GlobleC.Provider value={ { isMobileDevice } }>
+		<div className="wrapper">
+			<Header />
+		</div>
+	</GlobleC.Provider>
+	);
 }
 
 export default App;
