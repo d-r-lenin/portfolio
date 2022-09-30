0f3a5f019ca38f76e40572e582a5a98112682897
d-r-lenin <drlenin2002@gmail.com>
2022-09-30 18:40:00 +0530

    header-responsiveness

---
+++ client/src/js/script.js
@@ -0,0 +1,8 @@
+
+// menu toggle
+$(".burger-menu").click(function() {
+    $(".nav-bar").toggleClass("open");
+});
+$(".closing-cross").click(function() {
+    $(".nav-bar").toggleClass("open");
+});
