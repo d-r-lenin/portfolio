a2ab4edd67c06942bbaabe2ff053b42cea63ef0f
d-r-lenin <drlenin2002@gmail.com>
2022-11-19 12:48:45 +0530

    custom alerts, projects section, scroll snap

--- client/src/js/script.js
+++ client/src/js/script.js
@@ -3,101 +3,63 @@
 $(".burger-menu").click(function() {
     $(".nav-bar").toggleClass("open");
 });
+
 $(".closing-cross").click(function() {
+    $(".nav-bar").toggleClass("open");
+});
+
+$('.nav-bar a').click(function() {
     $(".nav-bar").toggleClass("open");
 });
 
 
 
-const pjc = $(".projects-body")
-const ar = [];
+// card flip
+const cards = $('.project-card');
 
-for (let index = 0; index < 1; index++) {
-    const element = document.createElement("div");
+$('.pj-nav-btn-l').click(function() {
+    const current = $('.selected-card');
+    console.log(current.prev());
+    if(current.prev().length === 0) {
+        showPopup('beginning of the list');
+        return;
+    }
+    current.toggleClass('selected-card');
+    current.prev().toggleClass('selected-card');
+});
 
-    element.classList.add(`pj-pad-${index}`);
+$('.pj-nav-btn-r').click(function() {
+    const current = $('.selected-card');
+    if(current.next().length === 0) {
+        showPopup('End of the list');
+        return;
+    }
+    current.toggleClass('selected-card');
+    current.next().toggleClass('selected-card');
+});
 
-    ar.push(element);
-    pjc.append(element);
+//  project card show all button
+$('.pj-show-all-btn').click(function() {
+    
+});
+
+
+// popup 
+function showPopup (msg) {
+    const popup = $(`#popup`);
+    popup.css('display', 'flex');
+    popup.text(msg);
+    popup.toggleClass('open');
+    setTimeout(closePopup, 3000);
 }
 
-
-addStyle(`
-.pj-pad-0 {
-    position: absolute;
-    height: 100px;
-    width: 100px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: red;
-    animation: move 2s ease-in-out infinite alternate;
-
-
-}
-
-@keyframes move {
-    0% {
-        transform: translate(-50%, -50%);
-    }
-    50% {
-        width: 200px;
-        height: 200px;
-        top: 0;
-        left: 0;
-    }
-    100% {
-        transform: translate(-50%, -50%) ;
-        width: 100px;
-        height: 100px;
-    }
-}
-
-@keyframes shadow {
-    0% {
-        transform: translate(-50%, -50%);
-    }
-    50% {
-        top: 20%;
-        width: 90px;
-        height: 90px;
-        filter: blur(20px);
-    }
-    100% {
-        transform: translate(-50%, -50%) ;
-        width: 100px;
-        height: 100px;
-    }
-}
-
-.pj-pad-1 {
-    position: absolute;
-    height: 100px;
-    width: 100px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: gray;
-    animation: shadow 2s ease-in-out infinite alternate;
-}
-
-`)
-
-
-// to add new style role to head
-function addStyle (style) {
-    const head = document.head;
-    const styleTag = document.createElement("style");
-    //to assign random id to style tag
-    styleTag.id = Math.random().toString(36).substring(7);
-    styleTag.innerHTML = style;
-    head.append(styleTag);
-    return styleTag.id;
-}
-
-
-// to remove style tag from head
-function removeStyle(id) {
-    const styleTag = document.getElementById(id);
-    styleTag.remove();
+function closePopup() {
+    const popup = $(`#popup`);
+    popup.toggleClass('open');
+    setTimeout(() => {
+        popup.text('');
+        setTimeout(() => {
+            popup.css('display', 'none');
+        }, 500);
+    }, 500);
 }
