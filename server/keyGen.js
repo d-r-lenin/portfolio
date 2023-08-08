22c2e2668ec8bd511222cd4a85ade567f5944663
d-r-lenin <drlenin2002@gmail.com>
2023-08-08 11:56:52 -0700

    secure

--- server/keyGen.js
+++ server/keyGen.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 async function checkAndCreateKeyFile(){
     if (!fs.existsSync("./key.json")) {
         console.log("Creating key.json file...");
-        console.log(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
         fs.writeFileSync("./key.json", process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
     }
 }
