12d0e0f6c9eb2466986dc32daa3c28fbc1ec8946
d-r-lenin <drlenin2002@gmail.com>
2023-08-08 11:52:51 -0700

    ping update

--- server/pinger.js
+++ server/pinger.js
@@ -1,13 +1,20 @@
 const corn = require("node-cron");
 const axios = require("axios");
 
-const url = process.env.PINGER_URL || "pinger-pinger.richardlenin.com";
+const url = process.env.PINGER_URL || "https://pinger-pinger.richardlenin.com/ping";
 
-const ping = () => {
-    axios.get(url).then((res) => {
-        console.log("pinging...");
-    });
+const ping = async() => {
+    try{
+        await axios.get(url)
+        console.log("ping ok");
+
+    }catch(err){
+        console.log(err);
+    } finally{
+        console.log("ping finally");
+    }
 };
 
 
-corn.schedule("*/5 * * * *", ping);
+// run every 10 seconds
+corn.schedule("*/10 * * * * *", ping);
